ed40861ae6d2389913786265d26a72223e584e3c
Lalu Kismara Hadi <n.kismara@gmail.com>
2019-11-08 03:22:46 +0700

    remove client id and secret

--- validasi_token.php
+++ validasi_token.php
@@ -1,9 +1,9 @@
 <?php 
 	require_once 'vendor/autoload.php';
+	include 'config.php'; //file contains client id and client secret
 
 	// Get $id_token via HTTPS POST.
-	$CLIENT_ID = '911706458791-0tu98raksnbn2v2uobo8n7c77u8qqanr.apps.googleusercontent.com';
-	$client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+	$client = new Google_Client(['client_id' => CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
 	$payload = $client->verifyIdToken($_POST['idtoken']);
 	if ($payload) {
 	  $userid = $payload['sub'];
